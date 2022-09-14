6320d187d73d1b5666edafd64c34e829aa6fb95d
Selim Ben Slimene <selimbenslimene@gmail.com>
2022-09-14 21:19:59 +0100

    addition-1

---
+++ 01-algorithms/find-mike.js
@@ -0,0 +1,10 @@
+//For an array “people” containing objects that have the following key/value pairs:
+//  {
+//	    name: “exampleName”,
+//	    age: 26,
+//	    jobTitle: “lawyer”,
+//	    pets: [“car”,”dog”,”rabbit”]
+//  } 
+//Write a function getMike(people), that returns the data for the person named “Mike”
+//Write a function hasCat(personObject) that returns if Mike (for example) has a cat
+//Write a function getAllCatOwners(people) that returns all cat owners
---
+++ 01-algorithms/how-many-students.js
@@ -0,0 +1,8 @@
+//For an array “people” containing objects that have the following key/value pairs:
+//    {
+//      name: “exampleName”,
+//      age: 26,
+//      jobTitle: “lawyer”,
+//      pets: [“car”,”dog”,”rabbit”]
+//    } 
+//Write a function that counts and then returns the number of students that exist in the people array
